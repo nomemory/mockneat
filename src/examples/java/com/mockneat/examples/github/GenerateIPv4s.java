d70df8d3c07b5ec33be7ec90091d0a2bc4c7c1db
Ciobanu, Andrei-Nicolin (UK - EDC) <andreinicolinciobanu@deloitte.co.uk>
2017-03-03 23:22:00 +0200

    Added examles for generating ipv4s.

---
+++ src/examples/java/com/mockneat/examples/github/GenerateIPv4s.java
@@ -0,0 +1,24 @@
+package com.mockneat.examples.github;
+
+import com.mockneat.mock.MockNeat;
+
+import static com.mockneat.types.enums.IPv4Type.CLASS_A;
+import static com.mockneat.types.enums.IPv4Type.CLASS_B;
+
+/**
+ * Created by andreinicolinciobanu on 03/03/2017.
+ */
+public class GenerateIPv4s {
+    public static void main(String[] args) {
+        MockNeat mock = MockNeat.threadLocal();
+
+        String ipv4 = mock.ipv4s().val();
+        System.out.println(ipv4);
+
+        String ipClassA = mock.ipv4s().type(CLASS_A).val();
+        System.out.println(ipClassA);
+
+        String classAorB = mock.ipv4s().types(CLASS_A, CLASS_B).val();
+        System.out.println(classAorB);
+    }
+}
