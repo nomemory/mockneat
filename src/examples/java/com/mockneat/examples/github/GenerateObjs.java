7d4c1f7f09ca2379e913e18e9e6de52b8bad3811
Ciobanu, Andrei-Nicolin (UK - EDC) <andreinicolinciobanu@deloitte.co.uk>
2017-03-04 12:41:22 +0200

    Added examples for generating Objects with objs(Class<T>)

---
+++ src/examples/java/com/mockneat/examples/github/GenerateObjs.java
@@ -0,0 +1,30 @@
+package com.mockneat.examples.github;
+
+import com.mockneat.examples.github.model.Test;
+import com.mockneat.mock.MockNeat;
+
+/**
+ * Created by andreinicolinciobanu on 04/03/2017.
+ */
+public class GenerateObjs {
+    public static void main(String[] args) {
+        MockNeat mock = MockNeat.threadLocal();
+
+        Test t = mock.objs(Test.class)
+                        .field("x", mock.strings().size(10))
+                        .field("y", mock.ints().range(100, 200))
+                        .field("z", mock.bools())
+                        .val();
+
+        System.out.println(t);
+
+        Test t2 = mock.objs(Test.class)
+                      .constructor(
+                        mock.strings().size(10),
+                        mock.ints().range(0, 10),
+                        mock.bools())
+                      .val();
+
+        System.out.println(t2);
+    }
+}
---
+++ src/examples/java/com/mockneat/examples/github/model/Test.java
@@ -0,0 +1,51 @@
+package com.mockneat.examples.github.model;
+
+/**
+ * Created by andreinicolinciobanu on 04/03/2017.
+ */
+public class Test {
+    private String x;
+    private Integer y;
+    private Boolean z;
+
+    public Test() {}
+
+    public Test(String x, Integer y, Boolean z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public String getX() {
+        return x;
+    }
+
+    public void setX(String x) {
+        this.x = x;
+    }
+
+    public Integer getY() {
+        return y;
+    }
+
+    public void setY(Integer y) {
+        this.y = y;
+    }
+
+    public Boolean getZ() {
+        return z;
+    }
+
+    public void setZ(Boolean z) {
+        this.z = z;
+    }
+
+    @Override
+    public String toString() {
+        return "Test{" +
+                "x='" + x + '\'' +
+                ", y=" + y +
+                ", z=" + z +
+                '}';
+    }
+}
