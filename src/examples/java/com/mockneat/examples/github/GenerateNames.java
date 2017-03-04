f90351976256ecd412117018c6b8342b67df9159
Ciobanu, Andrei-Nicolin (UK - EDC) <andreinicolinciobanu@deloitte.co.uk>
2017-03-04 21:15:08 +0200

    Added examples for generating Name(s).

---
+++ src/examples/java/com/mockneat/examples/github/GenerateNames.java
@@ -0,0 +1,29 @@
+package com.mockneat.examples.github;
+
+import com.mockneat.mock.MockNeat;
+
+import static com.mockneat.types.enums.NameType.FIRST_NAME_FEMALE;
+
+/**
+ * Created by andreinicolinciobanu on 04/03/2017.
+ */
+public class GenerateNames {
+    public static void main(String[] args) {
+        MockNeat mock = MockNeat.threadLocal();
+
+        String firstName = mock.names().type(FIRST_NAME_FEMALE).val();
+        System.out.println(firstName);
+
+        String first = mock.names().first().val();
+        System.out.println(first);
+
+        String last = mock.names().last().val();
+        System.out.println(last);
+
+        String full = mock.names().full().val();
+        System.out.println(full);
+
+        String fullWithInitial = mock.names().full(90).val();
+        System.out.println(fullWithInitial);
+    }
+}
