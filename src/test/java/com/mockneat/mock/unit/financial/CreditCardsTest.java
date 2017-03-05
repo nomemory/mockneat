1ffec5b3c434c923706256db354bea0fd3b4ff6f
Ciobanu, Andrei-Nicolin (UK - EDC) <andreinicolinciobanu@deloitte.co.uk>
2017-03-05 12:38:22 +0200

    Re-worked the tests used for CreditCards generation. Now the test coverage is 100%.

--- src/test/java/com/mockneat/mock/unit/financial/CreditCardsTest.java
+++ src/test/java/com/mockneat/mock/unit/financial/CreditCardsTest.java
@@ -2,21 +2,61 @@ package com.mockneat.mock.unit.financial;
 
 import com.mockneat.mock.utils.file.FileManager;
 import com.mockneat.types.enums.CreditCardType;
+import org.apache.commons.validator.routines.CreditCardValidator;
 import org.junit.Test;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static com.mockneat.mock.Constants.*;
 import static com.mockneat.mock.utils.LoopsUtils.loop;
 import static com.mockneat.mock.utils.LuhnUtils.luhnCheck;
+import static com.mockneat.types.enums.CreditCardType.*;
 import static com.mockneat.types.enums.DictType.CREDIT_CARD_NAMES;
 import static org.junit.Assert.assertTrue;
 
 public class CreditCardsTest {
 
     private FileManager FM = FileManager.getInstance();
+    private static Map<CreditCardType, Set<String>> PREFIX = new EnumMap<>(CreditCardType.class);
+
+    static {
+        Arrays.stream(CreditCardType.values())
+                .forEach(type -> {
+                    Set<String> typeSet = type.getPrefixes()
+                                                .stream()
+                                                .map(list ->
+                                                    list.stream()
+                                                        .map(i -> i.toString())
+                                                        .collect(Collectors.joining()))
+                                                .collect(Collectors.toSet());
+                    PREFIX.put(type, typeSet);
+                });
+    }
+
+    private static boolean hasValdiPrefix(String cc, CreditCardType type) {
+        return PREFIX.get(type)
+                        .stream()
+                        .filter(pref -> cc.startsWith(pref))
+                        .findFirst()
+                        .isPresent();
+    }
+
+    private static boolean isValidCCOfType(String cc, CreditCardType type) {
+        return type.getLength() == cc.length() &&
+                hasValdiPrefix(cc, type) &&
+                luhnCheck(cc);
+    }
+
+    @Test
+    public void testDefaultVal() throws Exception {
+        loop(
+                CCS_CYCLES,
+                MOCKS,
+                m -> m.creditCards().val(),
+                c -> assertTrue(isValidCCOfType(c, AMERICAN_EXPRESS))
+        );
+    }
 
     @Test(expected = NullPointerException.class)
     public void testCreditCardTypeNotNull() throws Exception {
@@ -32,56 +72,51 @@ public class CreditCardsTest {
     }
 
     @Test
-    public void testCreditCardCorrectLength() throws Exception {
-        loop(CCS_CYCLES, MOCKS, r -> {
-            CreditCardType creditCardType = r.from(CreditCardType.class).val();
-            String cc =  r.creditCards().type(creditCardType).val();
-            assertTrue(cc.length() == creditCardType.getLength());
-        });
-    }
-
-    @Test
-    public void testCreditCardHasCorrectPrefix() throws Exception {
-        loop(CCS_CYCLES, MOCKS, r -> {
-                    CreditCardType creditCardType = r.from(CreditCardType.class).val();
-                    // Obtain the set of prefixes associated with the credit card type
-                    Set<String> prefixes = creditCardType
-                                                .getPrefixes()
-                                                .stream()
-                                                .map(list ->
-                                                        list.stream()
-                                                                .map(i -> i.toString())
-                                                                .collect(Collectors.joining()))
-                                                .collect(Collectors.toSet());
-                    String cc = r.creditCards().type(creditCardType).val();
-                    // Test to see if the credit card
-                    // is starting with a correct prefix
-                    boolean test = false;
-                    for(String prefix : prefixes) {
-                        if (cc.startsWith(prefix)) {
-                            test = true;
-                            break;
-                        }
-                    }
-                    assertTrue(test);
-                });
-    }
-
-    @Test
-    public void testCreditCardAreValidLuhn() throws Exception {
-        loop(CCS_CYCLES,
+    public void testCreditCardTypes() throws Exception {
+        CreditCardType[] types = { MASTERCARD, VISA_13 };
+        loop(
+                CCS_CYCLES,
                 MOCKS,
-                r -> {
-                    CreditCardType c = r.from(CreditCardType.class).val();
-                    return r.creditCards().type(c).val();
-                },
-                c -> assertTrue(luhnCheck(c)));
+                m -> {
+                    String s = m.creditCards().types(types).val();
+                    assertTrue(isValidCCOfType(s, MASTERCARD) ||
+                               isValidCCOfType(s, VISA_13));
+                }
+        );
+    }
+
+    @Test
+    public void testCreditCards() throws Exception {
+        loop(
+                CCS_CYCLES,
+                MOCKS,
+                m -> {
+                    CreditCardType type = m.from(CreditCardType.class).val();
+                    String cc = m.creditCards().type(type).val();
+                    assertTrue(isValidCCOfType(cc, type));
+                }
+        );
+    }
+
+    @Test
+    public void testCreditCardsAmexApacheValidator() throws Exception {
+        CreditCardValidator ccv = new CreditCardValidator(CreditCardValidator.AMEX);
+        loop(
+                CCS_CYCLES,
+                MOCKS,
+                m -> m.creditCards().type(AMERICAN_EXPRESS).val(),
+                c -> assertTrue(ccv.isValid(c))
+        );
     }
 
     @Test
     public void testCreditCardNames() throws Exception {
         Set<String> set = new HashSet<>(FM.getLines(CREDIT_CARD_NAMES));
-        loop(CCS_CYCLES, MOCKS, r -> r.creditCards().names().val(), cn ->
-            assertTrue(set.contains(cn)));
+        loop(
+                CCS_CYCLES,
+                MOCKS,
+                r -> r.creditCards().names().val(),
+                cn -> assertTrue(set.contains(cn))
+        );
     }
 }
