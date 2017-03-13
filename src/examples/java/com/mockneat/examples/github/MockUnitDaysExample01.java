e48c4d9dd1bdde6c3282a98daa5ea7885cc32e4a
Ciobanu, Andrei-Nicolin (UK - EDC) <andreinicolinciobanu@deloitte.co.uk>
2017-03-13 23:11:14 +0200

    Added new example for generating random days

---
+++ src/examples/java/com/mockneat/examples/github/MockUnitDaysExample01.java
@@ -0,0 +1,24 @@
+package com.mockneat.examples.github;
+
+import com.mockneat.mock.MockNeat;
+
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+import static java.time.DayOfWeek.FRIDAY;
+
+/**
+ * Created by andreinicolinciobanu on 11/03/2017.
+ */
+public class MockUnitDaysExample01 {
+    public static void main(String[] args) {
+        MockNeat m = MockNeat.threadLocal();
+
+        String day = m.days()
+                        .after(FRIDAY)
+                        .display(TextStyle.FULL_STANDALONE, Locale.FRANCE)
+                        .val();
+
+        System.out.println(day);
+    }
+}
