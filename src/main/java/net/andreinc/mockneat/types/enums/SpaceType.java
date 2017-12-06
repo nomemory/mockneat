358926d8bb21045cc261c704f041f22bc4bb06b0
Ciobanu, Andrei-Nicolin (UK - EDC) <andreinicolinciobanu@deloitte.co.uk>
2017-12-06 11:08:53 +0200

    Added new MockUnitString -> space()

---
+++ src/main/java/net/andreinc/mockneat/types/enums/SpaceType.java
@@ -0,0 +1,8 @@
+package net.andreinc.mockneat.types.enums;
+
+public enum SPACE_TYPE {
+
+    CONSTELLATIONS;
+
+    private DictType[] dictTypes;
+}
---
+++ src/main/java/net/andreinc/mockneat/unit/text/Space.java
@@ -0,0 +1,7 @@
+package net.andreinc.mockneat.unit.text;
+
+/**
+ * Created by andreinicolinciobanu on 03/11/17.
+ */
+public class Space {
+}
